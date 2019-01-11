21fc154514a63e1c09fd54c3eadc757b58df9f5c
ngo.trung.thang <ngo.trung.thang@framgia.com>
2019-01-11 16:04:49 +0700

    Updates

---
+++ precache-manifest.4ee79bc74f9c363912f2a34f34f5d059.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab0783d5d9c0f0db6c3f",
+    "url": "/react-redux/static/js/runtime~main.ab0783d5.js"
+  },
+  {
+    "revision": "96cc435253b3e4dfa4dc",
+    "url": "/react-redux/static/js/main.96cc4352.chunk.js"
+  },
+  {
+    "revision": "bb75f11427a242583891",
+    "url": "/react-redux/static/js/1.bb75f114.chunk.js"
+  },
+  {
+    "revision": "bb75f11427a242583891",
+    "url": "/react-redux/static/css/1.5519f6ad.chunk.css"
+  },
+  {
+    "revision": "ff921e03ed094ed2b3cb40d24dc9d693",
+    "url": "/react-redux/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3dd5319f3ff294a711ddae543651f82a.js"
+  "/react-redux/precache-manifest.4ee79bc74f9c363912f2a34f34f5d059.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-redux/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
