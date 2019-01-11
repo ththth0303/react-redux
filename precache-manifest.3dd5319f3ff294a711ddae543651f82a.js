9e585b077d1955268b17340a49f6a015e91c602e
ngo.trung.thang <ngo.trung.thang@framgia.com>
2019-01-11 15:59:28 +0700

    Updates

---
+++ precache-manifest.3dd5319f3ff294a711ddae543651f82a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96cc435253b3e4dfa4dc",
+    "url": "/static/js/main.96cc4352.chunk.js"
+  },
+  {
+    "revision": "bb75f11427a242583891",
+    "url": "/static/js/1.bb75f114.chunk.js"
+  },
+  {
+    "revision": "bb75f11427a242583891",
+    "url": "/static/css/1.5519f6ad.chunk.css"
+  },
+  {
+    "revision": "94345c0f969df6eb3784103ffd3238d4",
+    "url": "/index.html"
+  }
+];
